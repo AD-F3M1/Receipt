2ff1c480a89bb3eb45528edf82bf94fe9b8bc1a9
Akitobi Oluwafemi <akitobioluwafemi@gmail.com>
2024-07-26 12:05:21 +0100

    add the recipt and update the styles and add scripts to the project.

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    
+    $("#form").submit(function(e){
+        e.preventDefault();
+
+        const inputtedName = $("#name").val();
+        const inputtedProduct = $("#product").val();
+        const inputtedDescription = $("#description").val();
+        const inputtedAmount = $("#amount").val();
+        const inputtedAddress = $("#address").val();
+        const inputtedEmail = $("#email").val();
+        const inputtedCountry = $("#country").val();
+        const inputtedState = $("#state").val();
+        const inputtedDate = $("#date").val();
+
+        $(".name").text(inputtedName);
+        $(".product").text(inputtedProduct);
+        $(".description").text(inputtedDescription);
+        $(".amount").text(inputtedAmount);
+        $(".address").text(inputtedAddress);
+        $(".email").text(inputtedEmail);
+        $(".country").text(inputtedCountry);
+        $(".state").text(inputtedState);
+        $(".date").text(inputtedDate);
+
+        $("#print-area").show();
+        $("#form").hide();
+
+    });
+});
+
+function printInvoice(){
+    window.print();
+}
